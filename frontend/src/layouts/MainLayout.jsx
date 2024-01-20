22bdc61c1a4c25738142fd14bffb431b2cd87355
Bugra <seyitbugraerden@gmail.com>
2024-01-20 03:47:57 +0300

    Pop-Up'lar ve Local Storage Eklendi

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import Header from "../components/Layouts/Header";
 import Footer from "../components/Layouts/Footer";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import Search from "../components/Modals/Search/Search";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
 function MainLayout({ children }) {
+  const [isSearchShow, setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow] = useState(false);
+
+  useEffect(() => {
+    const dialogStatus = localStorage.getItem("dialog")
+      ? JSON.parse(localStorage.getItem("dialog"))
+      : localStorage.setItem("dialog", JSON.stringify(true));
+
+    setTimeout(() => {
+      setIsDialogShow(dialogStatus);
+    }, 2000);
+  }, []);
+
   return (
-    <>
-      <Header />
+    <div className="main-layout">
+      <Header setIsSearchShow={setIsSearchShow} />
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
 
