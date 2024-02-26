862a133cce1eb2747d32f02c0738cd62a3dca60a
Bugra <seyitbugraerden@gmail.com>
2024-02-26 23:24:49 +0300

    backend station

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const post = 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello Express JS")
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://seyitbugraerden:ES1WpU0WnYzttZOF@mern-e-commerce.19qswqq.mongodb.net/"
+    );
+    console.log("connected");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello Express JS");
 });
-app.get("/api",(req,res)=>{
-    res.send("Hello I came from API")
+app.get("/api", (req, res) => {
+  res.send("Hello I came from API");
 });
 
-app.listen(5000,()=>{
-    console.log("Sunucu Çalışıyor")
-})
+app.listen(post, () => {
+  connect();
+  console.log("Sunucu Çalışıyor");
+});
