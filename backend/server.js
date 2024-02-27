834c9ace1bb68ca1becbd485650cad3e9f8161b3
Bugra <seyitbugraerden@gmail.com>
2024-02-27 12:39:01 +0300

    Package update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// import express from "express" yapısı js frameworkler için çalışırken backend node.js kısmında require ile import işlemi sağlıyoruz
+
+// nodemon adında bir npm kaynağı kurularak server'ı yeniden başlatmak yerine her save işlevinde server reload olabilir.
+
+const express = require("express"); // express npm install
+const mongoose = require("mongoose"); // mongoose npm install
+const dotenv = require("dotenv"); // dotenv npm install
 const app = express();
 const post = 5000;
 
@@ -8,7 +12,7 @@ dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URl);
     console.log("connected");
   } catch (error) {
     throw error;
@@ -21,8 +25,11 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.send("Hello I came from API");
 });
+app.get("/yarra", (req, res) => {
+  res.send("Hello I came from API");
+});
 
 app.listen(post, () => {
-  connect();
+  connect(); //connect fonsiyonu listen içerisinde yazılıyor.
   console.log("Sunucu Çalışıyor");
 });
