4f40944610578d638cf1e79470f2e121c7c4adfb
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-03 17:53:27 +0300

    Product Item Add To Cart

--- frontend/src/components/Products/ProductItem.jsx
+++ frontend/src/components/Products/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({ productItem }) => {
   const { addToCart, cartItems } = useContext(CartContext);
 
   const filteredCart = cartItems.find(
-    (cartItem) => cartItem._id == productItem.id
+    (cartItem) => cartItem._id == productItem._id
   );
 
   return (
